2cfe278c5e57a156797efe31514bce02f1b3b2c7
Shubhashish Verma <svashish305@gmail.com>
2021-02-20 19:02:15 +0530

    tested dash video stream in reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import VideoPlayer from './components/VideoPlayer';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>Dash Video Stream Test App</header>
+			<VideoPlayer />,
+		</div>
+	);
 }
 
 export default App;
